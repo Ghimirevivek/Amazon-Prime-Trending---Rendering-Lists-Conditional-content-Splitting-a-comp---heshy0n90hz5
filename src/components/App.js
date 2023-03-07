ed1df776712f458f28f37083255c8e194cd573f2
Ghimirevivek <108508449+Ghimirevivek@users.noreply.github.com>
2023-03-07 18:42:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,52 @@
-import React, {Component, useState} from "react";
+import React, {Component,useState } from "react";
 import '../styles/App.css';
+import { slides } from "../data";
 
 const App = () => {
-  return (
-    <></>
-  )
-}
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+  const currentSlide = slides[currentSlideIndex];
+  const isFirstSlide = currentSlideIndex === 0;
+  const isLastSlide = currentSlideIndex === slides.length - 1;
 
+  const handlePrevClick = () => {
+    setCurrentSlideIndex(currentSlideIndex - 1);
+  };
+
+  const handleNextClick = () => {
+    setCurrentSlideIndex(currentSlideIndex + 1);
+  };
+
+  const handleRestartClick = () => {
+    setCurrentSlideIndex(0);
+  };
+
+  return (
+    <div className="App">
+      <h1 data-testid="title">{currentSlide.title}</h1>
+      <p data-testid="text">{currentSlide.text}</p>
+      <button
+        data-testid="button-prev"
+        disabled={isFirstSlide}
+        onClick={handlePrevClick}
+      >
+        Prev
+      </button>
+      <button
+        data-testid="button-next"
+        disabled={isLastSlide}
+        onClick={handleNextClick}
+      >
+        Next
+      </button>
+      <button
+        data-testid="button-restart"
+        disabled={isFirstSlide}
+        onClick={handleRestartClick}
+      >
+        Restart
+      </button>
+    </div>
+  );
+};
 
 export default App;
